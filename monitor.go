e15d63338f3b4eade031f02214ffa9120a650f4a
Edgars Vasiljevs <edgars.vasiljevs@redboxdigital.com>
2016-05-09 00:36:39 +0700

    Added support for file monitoring over ssh

--- monitor.go
+++ monitor.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"github.com/hpcloud/tail"
+	"golang.org/x/crypto/ssh"
 	"os"
 	"regexp"
+	"strings"
 )
 
 type LogMessage [2]string
 
-// Local file monitoring
 func LocalFileMonitor(item []string, logs chan<- LogMessage) {
 
 	var err error
@@ -35,7 +38,56 @@ func LocalFileMonitor(item []string, logs chan<- LogMessage) {
 }
 
 func RemoteFileMonitor(item []string, logs chan<- LogMessage) {
-	// root:password@host:232:/filename
+
+	sshSplit := regexp.MustCompile(`([^@]+)@([^:]+(?::\d+|)):(.+)$`).FindStringSubmatch(item[1])
+
+	auth := strings.Split(sshSplit[1], ":")
+	sshConfig := ssh.ClientConfig{}
+
+	if len(auth) == 2 {
+		sshConfig.User = auth[0]
+		sshConfig.Auth = []ssh.AuthMethod{ssh.Password(auth[1])}
+	} else {
+		// TODO: pub key
+		sshConfig.User = auth[0]
+	}
+
+	address := strings.Split(sshSplit[2], ":")
+
+	// Add port if not defined
+	if len(address) == 1 {
+		address = append(address, "22")
+	}
+
+	// Connect to SSH server
+	connection, err := ssh.Dial("tcp", strings.Join(address, ":"), &sshConfig)
+	if err != nil {
+		Print(fmt.Sprintf("Failed to dial SSH: %s", err))
+		return
+	}
+
+	// Create new session
+	session, err := connection.NewSession()
+	if err != nil {
+		Print(fmt.Sprintf("Failed to create SSH session: %s", err))
+		return
+	}
+
+	// Close session once done
+	defer session.Close()
+
+	// Monitor ssh stdout
+	pipe, _ := session.StdoutPipe()
+	scanner := bufio.NewScanner(pipe)
+
+	go func() {
+		for scanner.Scan() {
+			logs <- LogMessage{item[0], scanner.Text()}
+		}
+	}()
+
+	// tail the file
+	session.Run("tail -f " + sshSplit[3])
 }
 
 func NewFileMonitor(config Config, logs chan<- LogMessage) {
